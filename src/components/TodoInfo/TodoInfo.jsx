8e1779c1b2989de7331bc7b82730d943c8375533
Ivan Solopenko <solopenko16@gmail.com>
2025-03-18 11:24:40 +0100

    Solution updated

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,9 +1,10 @@
-import { UserInfo } from '../UserInfo/UserInfo';
+import { UserInfo } from '../UserInfo';
 
 // Add the required props
 export const TodoInfo = ({ todo }) => (
   <article
     className={`TodoInfo TodoInfo--${todo.completed ? 'completed' : ''}`}
+    key={todo.id}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
